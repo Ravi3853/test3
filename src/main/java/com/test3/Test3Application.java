08e427360884ae34f5be5db8e01cd4de564813d6
Ravi3853 <rhugond@gmail.com>
2025-04-13 10:49:19 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test3Application {
 
 	public static void main(String[] args) {
+		string name="mike";
 		SpringApplication.run(Test3Application.class, args);
 	}
 
