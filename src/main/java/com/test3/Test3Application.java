bb8dc7d6ca56197e2579fcd71c9e61708bf73cd0
Ravi3853 <rhugond@gmail.com>
2025-04-13 11:37:30 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -9,6 +9,7 @@ public class Test3Application {
 	public static void main(String[] args) {
 		string name="mike";
 		int x=100;
+		boolean isPresent=false;
 		SpringApplication.run(Test3Application.class, args);
 	}
 
